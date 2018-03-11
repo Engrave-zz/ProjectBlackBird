17e1c952b5de4158a2343f22154e68bc472fcbbc
Brandon Samuel <Brandon.Samuel2010@gmail.com>
2018-03-10 23:03:12 -0500

    Updated Project

---
+++ WSC.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WSC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
